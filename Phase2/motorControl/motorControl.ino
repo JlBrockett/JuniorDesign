712ff1db69f7c3b335f114e476c538a305d2656a
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2024-02-27 12:57:33 -0500

    Final Update

--- Phase2/motorControl/motorControl.ino
+++ Phase2/motorControl/motorControl.ino
@@ -6,9 +6,11 @@ February 16th, 2024
 EE31 Phase 2A
 */
 
-const int motorPin1 = 3;
-const int motorPin2 = 4;
-const int enablePin1 = 9;
+const int motorPin1 = 5;
+const int motorPin2 = 6;
+const int enablePin1 = 3;
+
+static unsigned long controlMillis = millis();
 
 
 void setup() {
@@ -20,44 +22,50 @@ void setup() {
 }
 
 void loop() {
-  
-  static unsigned long controlMillis = millis();
 
-  if (millis() - controlMillis > 0 and millis() - controlMillis < 20000) {
-    directionOne()
+  if (millis() - controlMillis < 5000) {
+    forwardDirection();
   }
-  else if (millis() - controlMillis > 20000 and millis() - controlMillis < 40000)
+  else if (millis() - controlMillis > 5000 and millis() - controlMillis < 7000) {
+    stopMotor();
+  }
+  else if (millis() - controlMillis > 7000 and millis() - controlMillis < 12000) {
+    reverseDirection();
+  }
+  else if (millis() - controlMillis > 12000) {
+    controlMillis = millis();
+  }
 
 }
 
 
 /*
- * name:      directionOne
+ * name:      forwardDirection
  * purpose:   move the motor
  * arguments: None
  * returns:   None (void)
  * effects: moves the motor forward
  */
-void directionOne() {
+void forwardDirection() {
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, LOW);
-  analogWrite(enablePin1, 127); // Set speed (0 to 255)
-  delay(2000); // Run for 2 seconds
+  analogWrite(enablePin1, 255); // Set speed (0 to 255)
+  // delay(2000); // Run for 2 seconds
 }
 
 
 /*
- * name:      directionTwo
+ * name:      reverseDirection
  * purpose:   move the motor
  * arguments: None
  * returns:   None (void)
  * effects: moves the motor backward
  */
-void directionTwo() {
+void reverseDirection() {
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, HIGH);
-  analogWrite(enablePin1, 127); // Set speed (0 to 255)
-  delay(2000); // Run for 2 seconds
+  analogWrite(enablePin1, 255); // Set speed (0 to 255)
+  // delay(2000); // Run for 2 seconds
 }
 
 
@@ -70,5 +78,4 @@ void directionTwo() {
 void stopMotor() {
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
-  delay(1000); // Stop for 1 second
 }
