30de50057614e8482bebdfd95162bc957f7e4cb3
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2024-03-28 13:08:02 -0400

    Updated battery level checker

--- Phase3/Battery Management and Measuring System/voltageManagement/voltageManagement.ino
+++ Phase3/Battery Management and Measuring System/voltageManagement/voltageManagement.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+const int analogPin = A0; 
+const float resistorOne = 2000.0; //Assumed value for R1
+const float resistorTwo = 1000.0; //Assumed value for R2
 
+
+void setup() {
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  int sensorValue = analogRead(analogPin);
+  float voltage = sensorValue * (5.00 / 1023.00); // Convert to voltage
+  float batteryVoltage = voltage * ((resistorOne + resistorTwo) / resistorTwo);
+
+  Serial.print("Battery Voltage: ");
+  Serial.println(batteryVoltage);
+
+  
+  //Battery low; might need to measure that before we set a threshold
+  if (batteryVoltage <= 6.0) { 
+    Serial.println("Battery low, consider replacing.");
+  }
+
+  delay(1000);
 }
