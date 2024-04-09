c342882f76021b8dff58a2355b501dc760d6fad6
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2024-04-09 12:19:45 -0400

    Sketch for calibrating color thresholds

---
+++ Phase3/calibrationSketch/calibrationSketch.ino
@@ -0,0 +1,27 @@
+const int redLEDPin = 9; 
+const int blueLEDPin = 10;
+const int sensorPin = A0; 
+
+void setup() {
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(blueLEDPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(redLEDPin, HIGH);
+  delay(100); // Stabilization delay
+  Serial.print("Red LED reading: ");
+  Serial.println(analogRead(sensorPin));
+  digitalWrite(redLEDPin, LOW);
+
+  delay(500); // Wait before next reading
+
+  digitalWrite(blueLEDPin, HIGH);
+  delay(100); // Stabilization delay
+  Serial.print("Blue LED reading: ");
+  Serial.println(analogRead(sensorPin));
+  digitalWrite(blueLEDPin, LOW);
+
+  delay(500);
+}
