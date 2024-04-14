b137b94e7439a8037cb58ff570462ad06faad37e
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2024-04-14 15:07:16 -0400

    New update with getCommand command

--- Phase4/communicationsProtocol/communicationsProtocol.ino
+++ Phase4/communicationsProtocol/communicationsProtocol.ino
@@ -1,16 +1,36 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <ArduinoSTL.h>
+#include <vector>
 
-char ssid[] = SECRET_SSID; 
-char pass[] = SECRET_PASS;
-const char postRoute[] = "POST /1B9564ECC320/1B9564ECC321 HTTP/1.1"; 
+//Our UUID: 56FC703ACE1A
+//Cp-team UUID: 828BD9E1B7C7
 
+char ssid[] = "junior"; 
+char pass[] = "designdesign";
 int status = WL_IDLE_STATUS;
+char server[] = "ee31.ece.tufts.edu";    // name address for ee31 (using DNS)
 
-char server[] = "ee31.ece.tufts.edu";
 WiFiClient client;
 
+char messageData[100];
+char getRoute[] = "GET /828BD9E1B7C7/828BD9E1B7C7 HTTP/1.1";
+char postBody[] = "variable1=date&variable2=value2&variable3=value3&variable4=value4&variable5=value5&variable6=light";
+char postRoute[] = "POST /828BD9E1B7C7/828BD9E1B7C7 HTTP/1.1";
+
+// variables
+int amp_Count = 0;
+std::vector<char> command;
+int idx = 0;
+int startRead = 0;
+int LED_pin = 7;
+
+
+
+
 void setup() {
+
+  pinMode(LED_pin, OUTPUT);
   Serial.begin(9600);
 
   while (!Serial) {
@@ -20,58 +40,36 @@ void setup() {
   }
 
   // check for the WiFi module:
-
   if (WiFi.status() == WL_NO_MODULE) {
-
     Serial.println("Communication with WiFi module failed!");
-
-    // don't continue
-
     while (true);
-
   }
-
-  String fv = WiFi.firmwareVersion();
-
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-
-    Serial.println("Please upgrade the firmware");
-
-  }
-
 
   while (status != WL_CONNECTED) {
-
     Serial.print("Attempting to connect to SSID: ");
-
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
-    delay(10000);
 
+    printWifiStatus();
+    delay(10000);
   }
 
-  Serial.println("Connected to wifi");
+  Serial.println("Connected to WiFi");
   printWifiStatus();
+
+
+
 
   Serial.println("\nStarting connection to server...");
 
-  // if you get a connection, report back via serial:
-
   if (client.connect(server, 80)) {
-
     Serial.println("connected to server");
-
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-
-    client.println("Host: www.google.com");
-
-    client.println("Connection: close");
-
-    client.println();
-
   }
+
 }
+
+
+
 
 void loop() {
 
@@ -79,84 +77,202 @@ void loop() {
 
   // from the server, read them and print them:
 
-  while (client.available()) {
-
+  while (client.available()){
     char c = client.read();
+    if (startRead == 8){
+      messageData[idx] = c;
+      idx++;
+    }
+
+    if (c == '&'){
+      startRead++;
+    }
 
     Serial.write(c);
-
   }
 
   // if the server's disconnected, stop the client:
-
   if (!client.connected()) {
 
     Serial.println();
-
+    printMessage(messageData);
+    readCommand(messageData);
     Serial.println("disconnecting from server.");
 
     client.stop();
 
-    // do nothing forevermore:
-
     while (true);
 
   }
+
+  POSTServer(postRoute, postBody);
+  GetServer(getRoute, messageData);
 }
+
+
+
+
+
 
 void printWifiStatus() {
 
   // print the SSID of the network you're attached to:
 
   Serial.print("SSID: ");
-
   Serial.println(WiFi.SSID());
 
   // print your board's IP address:
-
   IPAddress ip = WiFi.localIP();
-
   Serial.print("IP Address: ");
-
   Serial.println(ip);
 
   // print the received signal strength:
-
   long rssi = WiFi.RSSI();
-
   Serial.print("signal strength (RSSI):");
-
   Serial.print(rssi);
-
-  Serial.println(" dBm");
+  Serial.println("dBm");
 }
 
-void GETServer(const char theRoute[], char *message) { 
- 
-  if (client.connect(server, 80)) { 
-    // Make a HTTP GET request: 
-    client.println(theRoute); 
-    client.print("Host: "); 
-    client.println(server); 
-    client.println("Connection: close"); 
-    client.println(); 
-    GetMessageBody(); // get the message from the server 
-    // place in appropriate form and variable 
-    // if the server's disconnected, stop the client: 
-    if (!client.connected()) { 
-      client.stop() 
-    } 
-  } 
+
+
+
+void GetServer(const char getRoute[], char *message) {
+
+  if (client.connect(server, 80)) {
+
+    // make an HTTP GET request:
+    client.println(getRoute);
+    client.print("Host: ");
+    client.println(server);
+    client.println("Connection:close");
+    client.println();
+
+    int idx = 0;
+
+    //&& idx < sizeof(messageData -1)
+    // while (client.available()) {
+    //   char c = client.read();
+    //   Serial.write(c);
+    //   messageData[idx] = c;
+    //   idx++;
+    // }
+
+    for(int i = 0; i < 100; i++){
+      char c = client.read();
+      message[i] = c;
+    }
+
+    //messageData[227] = '\0';
+    //getCommand(messageData);
+    //printCommand(command);
+    //printMessage(message);
+    //readCommand();
+
+    delay(10000);
+
+    // if server is disconnected, stop the client:
+    if(!client.connected()) {
+      client.stop();
+    }
+  }
 }
 
-void POSTServer(const char theRoute[], char *bodyMessage) {  
-  client.println(theRoute); 
-  client.print("Host: "); 
-  client.println(server); 
-  client.println("Content-Type: application/x-www-form-urlencoded"); 
-  client.print("Content-Length: "); 
-  int postBodyLength = strlen(bodyMessage); 
-  client.println(postBodyLength); 
-  client.println(); 
-  client.print(bodyMessage); 
-} 
+
+
+
+void GetMessageBody(char *message) {
+
+
+}
+
+
+
+
+void POSTServer(const
+  char getRoute[],
+  char *bodyMessage) {
+
+
+
+
+  if (client.connect(server, 80)) {
+    client.println(getRoute);
+    client.print("Host: ");
+    client.println(server);
+    client.println("Content-Type:application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    int postBodyLength = strlen(bodyMessage);
+    client.println(postBodyLength);
+    client.println();
+    client.print(bodyMessage);
+    if(!client.connected()) {
+      client.stop();
+    }
+  }
+}
+
+
+
+
+void getCommand(char *message) {
+
+  int ampCount = 0;
+  int idx = 0;
+
+  while (message[idx] != '\0') {
+
+    if (message[idx] == '&') {
+      ampCount ++;
+    }
+
+    if (ampCount >= 8) {
+      command.push_back(message[idx]);
+    }
+
+    idx++;
+  }
+}
+
+
+
+
+void printCommand(std::vector<char> command) {
+
+  for (int i = 0; i < command.size(); i++) {
+
+    Serial.print(command[i]);
+
+  }
+
+}
+
+
+
+
+void printMessage(char *message) {
+
+  for (int i = 0; i < 15; i++) {
+
+    Serial.print(message[i]);
+
+  }
+
+}
+
+
+
+
+void readCommand(char *message) {
+
+  if (message[10] == 'l') {
+
+    digitalWrite(LED_pin, HIGH);
+
+    delay(1000);
+
+    digitalWrite(LED_pin, LOW);
+
+  }
+
+}
+
