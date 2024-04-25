798715d38b73f7e18a51123e932dc2793ae43547
Mo-Selasie <153629461+Mo-Selasie@users.noreply.github.com>
2024-04-25 14:02:07 -0400

    updated challenge codes

--- Challenges/challengeFive/challengeFive.ino
+++ Challenges/challengeFive/challengeFive.ino
@@ -1,9 +1,76 @@
+/*
+motorControl.ino
+Alex, Jane, Selasie, and Supriti
+April 25th, 2024
+
+EE31 Challenge 5
+*/
+
+//motor one
+const int motorAPin1 = 9;
+const int motorAPin2 = 10;
+const int enableAPin1 = 11;
+
+//motor two
+const int motorBPin1 = 5;
+const int motorBPin2 = 6;
+const int enableBPin1 = 3;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(motorAPin1, OUTPUT);
+  pinMode(motorAPin2, OUTPUT);
+  pinMode(enableAPin1, OUTPUT);
+
+  pinMode(motorBPin1, OUTPUT);
+  pinMode(motorBPin2, OUTPUT);
+  pinMode(enableBPin1, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //move forward 25 inches
+  forwardDirection();
+  delay(1600);
 
+  //Stop
+  stopMotor();
+  delay(1000);
+}
+
+
+/*
+ * name:      forwardDirection
+ * purpose:   move the motor
+ * arguments: motor speed
+ * returns:   None (void)
+ * effects: moves the motor forward
+ */
+void forwardDirection() {
+  analogWrite(motorAPin1, 150);
+  digitalWrite(motorAPin2, LOW);
+
+  digitalWrite(motorBPin1, LOW);
+  analogWrite(motorBPin2, 150);
+
+  digitalWrite(enableAPin1, HIGH); // Set speed (0 to 255)
+  digitalWrite(enableBPin1, HIGH);
+}
+
+
+/*
+ * name:      stopMotor
+ * purpose:   stop the motor
+ * arguments: None
+ * returns:   None (void)
+ */
+void stopMotor() {
+  //stop motor A
+  digitalWrite(motorAPin1, LOW);
+  digitalWrite(motorAPin2, LOW);
+
+  //stop motor B
+  digitalWrite(motorBPin1, LOW);
+  digitalWrite(motorBPin2, LOW);
 }
--- Challenges/challengeTwo/challengeTwo.ino
+++ Challenges/challengeTwo/challengeTwo.ino
@@ -1,9 +1,160 @@
+/*
+motorControl.ino
+Alex, Jane, Selasie, and Supriti
+April 25th, 2024
+
+EE31 Challenge 2
+*/
+
+//motor one
+const int motorAPin1 = 9;
+const int motorAPin2 = 10;
+const int enableAPin1 = 11;
+
+//motor two
+const int motorBPin1 = 5;
+const int motorBPin2 = 6;
+const int enableBPin1 = 3;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(motorAPin1, OUTPUT);
+  pinMode(motorAPin2, OUTPUT);
+  pinMode(enableAPin1, OUTPUT);
+
+  pinMode(motorBPin1, OUTPUT);
+  pinMode(motorBPin2, OUTPUT);
+  pinMode(enableBPin1, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //move forward twelve inches
+  forwardDirection();
+  delay(1600);
 
+  //Stop
+  stopMotor();
+  delay(1000);
+
+  //turn around 180°
+  turn();
+  delay(1160);
+
+  //stop, move backwards three inches
+  stopMotor();
+  delay(1000);
+  reverseDirection();
+  delay(800);
+
+  //stop, turn left, turn right, turn right, and turn right to end up as close as possible to the  bot’s starting position.
+  stopMotor();
+  delay(1000);
+
+  turn();
+  delay(580);
+
+  stopMotor();
+  delay(1000);
+
+  turnRight();
+  delay(580);
+
+  stopMotor();
+  delay(1000);
+
+  turnRight();
+  delay(580);
+
+  stopMotor();
+  delay(1000);
+
+  turnRight();
+  delay(580);
+
+  stopMotor();
+  delay(1000);
+
+  reverseDirection();
+  delay(2000);
+
+
+
+  stopMotor();
+  delay(10000);
+}
+
+
+/*
+ * name:      forwardDirection
+ * purpose:   move the motor
+ * arguments: motor speed
+ * returns:   None (void)
+ * effects: moves the motor forward
+ */
+void forwardDirection() {
+  analogWrite(motorAPin1, 150);
+  digitalWrite(motorAPin2, LOW);
+
+  digitalWrite(motorBPin1, LOW);
+  analogWrite(motorBPin2, 150);
+
+  digitalWrite(enableAPin1, HIGH); // Set speed (0 to 255)
+  digitalWrite(enableBPin1, HIGH);
+}
+
+
+/*
+ * name:      reverseDirection
+ * purpose:   move the motor
+ * arguments: motor speed
+ * returns:   None (void)
+ * effects: moves the motor backward
+ */
+void reverseDirection() {
+  digitalWrite(motorAPin1, LOW);
+  analogWrite(motorAPin2, 150);
+  
+  analogWrite(motorBPin1, 150);
+  digitalWrite(motorBPin2, LOW);
+
+  digitalWrite(enableAPin1, HIGH); //right min 130
+  digitalWrite(enableBPin1, HIGH); // left
+}
+
+void turn() {
+  analogWrite(motorAPin1, 150);
+  digitalWrite(motorAPin2, LOW);
+  digitalWrite(enableAPin1, HIGH); 
+
+  analogWrite(motorBPin1, 150);
+  digitalWrite(motorBPin2, LOW);
+  digitalWrite(enableBPin1, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(motorAPin1, LOW);
+  analogWrite(motorAPin2, 150);
+  digitalWrite(enableAPin1, HIGH); 
+
+  digitalWrite(motorBPin1, LOW);
+  analogWrite(motorBPin2, 150);
+  digitalWrite(enableBPin1, HIGH);
+}
+
+/*
+ * name:      stopMotor
+ * purpose:   stop the motor
+ * arguments: None
+ * returns:   None (void)
+ */
+void stopMotor() {
+  //stop motor A
+  digitalWrite(motorAPin1, LOW);
+  digitalWrite(motorAPin2, LOW);
+
+  //stop motor B
+  digitalWrite(motorBPin1, LOW);
+  digitalWrite(motorBPin2, LOW);
 }
